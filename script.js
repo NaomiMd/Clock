f2ddc106aee99d7e8d4ab49d78b1930a1804d87a
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-10-24 17:48:08 +0200

    add var to script

---
+++ script.js
@@ -0,0 +1,8 @@
+//variable//
+let heures = document.getElementById('hour');
+let minutes = document.getElementById('minutes');
+let secondes = document.getElementById('secondes');
+let jour = document.getElementById('day');
+let chiffre = document.getElementById('number');
+let mois = document.getElementById('month');
+let année = document.getElementById('year');
